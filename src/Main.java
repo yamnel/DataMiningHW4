af7e49b54c9e43971a24c5238729dfec9a48ddae
Yamnel <yamnel@users.noreply.github.com>
2017-04-12 18:35:03 -0400

    Parsed the all words from both training and testing data and created the frequency of each word for every training email.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
+import sun.text.resources.iw.FormatData_iw_IL;
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class Main {
+
     // Arrays of files that hold the trainData & testData.txt names
-    static File[] trainingFiles;
-    static File[] testingFiles;
+    static File[] trainingFilesList;
+    static File[] testingFilesList;
+    static ArrayList<File> fileNames = new ArrayList();
 
     // Dictionary that holds the words in train and test and their count
-    static HashMap<String, Integer> trainWords = new HashMap();
-    static HashMap<String, Integer> testWords = new HashMap();
+    static ArrayList<String> allWords = new ArrayList<>();
+    static int[][] emailWordFreq;
+    static int numberOfTrainingEmails;
 
 
     public Main() {
@@ -23,47 +28,64 @@ public class Main {
         File testingFolderPath = new File(System.getProperty("user.dir") + "/testData");
 
         // loading the .txt names
-        trainingFiles = trainingFolderPath.listFiles();
-        testingFiles = testingFolderPath.listFiles();
+        trainingFilesList = trainingFolderPath.listFiles();
+        numberOfTrainingEmails = trainingFilesList.length;
 
+        testingFilesList = testingFolderPath.listFiles();
 
-        // loading the .txt data into the Dictionaries
-        loadData(trainingFiles, trainWords);
-        loadData(testingFiles, testWords);
+        // loading the .txt data into the lists
+        parseWords(trainingFilesList);
+        parseWords(testingFilesList);
 
-        // TESTING PRINT
-        System.err.printf("There are %d training data and %d test data words\n\n", trainWords.size(), testWords.size());
-        trainWords.keySet().forEach((key) -> System.out.println(key + " -> " + trainWords.get(key)));
-        testWords.keySet().forEach((key) -> System.out.println(key + " -> " + testWords.get(key)));
+        emailWordFreq = new int[numberOfTrainingEmails][allWords.size() + 1];
+        loadWordFreq(trainingFilesList);
+
     }
 
-    // increases count of @word in @dictionary
-    static void increaseCountOf(String word, HashMap<String, Integer> dictionary) {
-        if (dictionary.containsKey(word)) {
-            Integer temp = dictionary.get(word);
-            temp = temp + 1;
-            dictionary.put(word, temp);
-        } else {
-            dictionary.put(word, 1);
+
+    static void loadWordFreq(File[] fileNames) {
+        Scanner lineScanr = null;
+//        for (File file : fileNames) {
+        for(int i = 0; i < fileNames.length;i++){
+            try {
+                lineScanr = new Scanner(fileNames[i]); // load the line
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            while (lineScanr.hasNext()) {
+                Scanner wordScanr = new Scanner(lineScanr.nextLine()); // load the word
+
+                while (wordScanr.hasNext()) { // for every word
+                    String word = wordScanr.next();
+
+                    int index = allWords.indexOf(word);
+                    if (isAlphaNumeric(word)) emailWordFreq[i][index]++;
+                }
+            }
+            if(isItSpam(fileNames[i].getName())){
+                emailWordFreq[i][emailWordFreq.length-1] = 1;
+            }
         }
-
     }
-
 
     // loads data from @containingFolders into @dictionary
-    static void loadData(File[] containingFolder, HashMap<String, Integer> dictionary) {
+    static void parseWords(File[] containingFolder) {
         Scanner lineScanr = null;
 
         // go through all .txt files
-        for (int fileNumber = 0; fileNumber < containingFolder.length; ++fileNumber) {
+        for (int fileNumber = 0; fileNumber < containingFolder.length; fileNumber++) {
             File file = containingFolder[fileNumber];
 
             if (file.isFile()) {  // if the file is valid
+                fileNames.add(file);
+
                 try {
                     lineScanr = new Scanner(file); // load the line
-                } catch (FileNotFoundException var9) {
-                    var9.printStackTrace();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
                 }
+
 
                 while (lineScanr.hasNext()) { // for every line
                     Scanner wordScanr = new Scanner(lineScanr.nextLine()); // load the word
@@ -71,14 +93,19 @@ public class Main {
                     while (wordScanr.hasNext()) { // for every word
                         String word = wordScanr.next();
 
-                        if (isAlphaNumeric(word)) {
-                            increaseCountOf(word, dictionary);
+                        if (!allWords.contains(word) && isAlphaNumeric(word)) {
+                            allWords.add(word);
                         }
-                    }
-                }
-            }
-        }
 
+                    }// end of words loop
+                }// end of line loop
+            }
+        }// end of .txt loop
+    }
+
+
+    static boolean isItSpam(String filename) {
+        return filename.startsWith("sp");
     }
 
     static boolean isAlphaNumeric(String str) { // checks for alphanumerics on apostrophes
